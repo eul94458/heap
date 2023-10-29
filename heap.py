a990a9b53d1a502a5b67090e81b63c4cf652ba2a
eul94458 <62548460+eul94458@users.noreply.github.com>
2023-10-30 00:35:59 +0900

    Update heap.py

--- heap.py
+++ heap.py
@@ -2,52 +2,60 @@
 from math import log, ceil, floor
 from collections import defaultdict, deque
 from operator import le, lt, ge, gt
-
+from copy import deepcopy as dcopy, copy
 
 class Heap:
-    
-    NAME = 'heap'
-    
+
+    NAME = 'Heap'
+
     def __init__(self, *things):
-        self.h = deque()
+        self.data = deque()
+        self.dict = {}
         self += things
-            
+
     def __iadd__(self, iterable):
         for x in iterable:
             self.append(x)
         return self
-        
+
+    def copy(self):
+        return copy(self)
+
+    def deepcopy(self):
+        return dcopy(self)
+
     def __len__(self):
-        return len(self.h)
-        
+        return self.data.__len__()
+
     def __iter__(self):
-        for x in self.h:
-            yield x
-        
-    def _packstr(self, a_deque):
-        a_deque.appendleft(f"{self.NAME} {'v'*12}")
-        a_deque.append('^'*20)
-        return a_deque
-    
+        new = self.copy()
+        while new:
+            yield new.popleft()
+        return None
+
     def __repr__(self):
         return self.__str__()
-    
+
     def __str__(self):
-        pack = self._packstr
-        h = self.h
-        n_layers = ceil( log( len(h)+1, 2 ) )
+        def wraping(a_deque):
+            a_deque.appendleft(f"{self.NAME} {'v'*12}")
+            a_deque.append('^'*20)
+            return a_deque
+
+        data = self.data
+        n_layers = ceil( log( len(data)+1, 2 ) )
 
         if n_layers < 1:
-            return '\n'.join(pack(deque()))
+            return '\n'.join(wraping(deque()))
         elif n_layers == 1:
-            stack = deque([str(h[0])])
-            stack = pack(stack)
+            stack = deque([str(data[0])])
+            stack = wraping(stack)
             return '\n'.join(stack)
         else:
             stack = deque()
             ele_per_layer = [2**x for x in range(n_layers)]
             # +2 account for space encompass each element
-            span_per_ele = max(len(str(x)) for x in h) + 1
+            span_per_ele = max(len(str(x)) for x in data) + 1
             # The real number of element for the last layer is in [-1]
             graph_width = ele_per_layer[-1] * (span_per_ele)
             start = 0
@@ -55,96 +63,119 @@ class Heap:
                 end = start + epl
                 ele_span = graph_width // epl
                 # every element is center-aligned
-                this_layer = ''.join( f"{x:^{ele_span}}" for x in list(h)[start:end] )
+                this_layer = ''.join( f"{str(x):^{ele_span}}" for x in list(data)[start:end] )
                 stack.append(this_layer)
                 start = end
-            stack = pack(stack)
+            stack = wraping(stack)
             # right-align all layers
             return '\n'.join(f"{row:<{graph_width}}" for row in stack)
 
     def append(self, x):
-        self.h.append(x)
-        self.up(len(self.h)-1)
+        self.data.append(x)
+        idx = self.data.__len__() - 1
+        self.dict[x] = idx
+        self.up(idx)
         return self
 
     def appendleft(self, x):
-        self.h.appendleft(x)
+        self.data.appendleft(x)
+        self.dict[x] = 0
+        self.down([0])
         return self
 
     def pop(self):
-        return self.h.pop()
-    
+        x = self.data.pop()
+        del self.dict[x]
+        # TODO: potential bug if there are multiple entries for the same key
+        return x
+
     def popleft(self):
         # Must have at least one element to pop()
-        h = self.h
-        if len(self.h) >= 1:
-            h[0], h[-1] = h[-1], h[0]
-            out = h.pop()
-            if len(h) > 0:
+        data = self.data
+        if len(data) >= 1:
+            data[0], data[-1] = data[-1], data[0]
+            out = data.pop()
+            if len(data) > 0:
                 self.down(0)
             return out
         else:
-            raise ValueError("Heap is empty.")
-    
-    def copy(self):
-        from copy import copy
-        return copy(self)
-    
-    def deepcopy(self):
-        from copy import deepcopy
-        return deepcopy(self)
-    
-    def up(self, child_k):
-        h = self.h
-        compare = self.UP_COMPARE
-        child_v = h[child_k]
-        # If index i == 0, no need to do anthing.
-        while child_k >= 1:
-            parent_k = (child_k-1)//2
-            parent_v = h[parent_k]
-            if compare(child_v, parent_v):
-                h[child_k] = parent_v
-                child_k = parent_k
-            else:
-                break
-        h[child_k] = child_v
+            raise IndexError("Heap is empty.")
 
-    def down(self, parent_k):
-        max_or_min, compare = self.MAX_OR_MIN, self.DOWN_COMPARE
-        h = self.h
-        end = len(h)-1
-        parent_v = h[parent_k]
-        previous_parent_k = -1
-        while previous_parent_k != parent_k:
-            previous_parent_k = parent_k
-            lchild_k = parent_k*2+1
-            rchild_k = lchild_k+1
-            # Child may not exist for one node,
-            #  so need to check before getting the value
-            if lchild_k <= end:
-                lchild_v = h[lchild_k]
-                if rchild_k <= end:
-                    rchild_v = h[rchild_k]
-                    max_v = max_or_min(lchild_v, rchild_v)
-                    swap_k, swap_v = (
-                        (lchild_k, lchild_v) 
-                        if max_v==lchild_v else
-                        (rchild_k, rchild_v)
-                        )
-                    h[parent_k], h[swap_k] = swap_v, parent_v
-                    parent_k = swap_k
-                else:
-                    # Only exist lchild, so only compare to lchild.
-                    if compare(parent_v, lchild_v):
-                        h[parent_k], h[lchild_k] = lchild_v, parent_v
-                        parent_k = lchild_k
+    def up(self, child_i):
+        data = self.data
+        dic = self.dict
+        compare = self.UP_COMPARE
+        child_v = data[child_i]
+        # If index i == 0, no need to do anthing.
+        while child_i > 0:
+            parent_i = (child_i-1)//2
+            parent_v = data[parent_i]
+            if compare(child_v, parent_v):
+                data[child_i] = parent_v
+                dic[parent_v] = child_i
+                child_i = parent_i
+            else:
+                # Can no longer move upwards
+                break
+        data[child_i] = child_v
+        dic[child_v] = child_i
+
+    def down(self, parent_i):
+        compare = self.DOWN_COMPARE
+        data = self.data
+        dic = self.dict
+        end = len(data)-1
+        if parent_i == end:
+            return None
+        if parent_i > end:
+            raise IndexError(f"This index exceed the length: {parent_i}")
+        parent_v = data[parent_i]
+        child_i = parent_i
+        while True:
+            lchild_i = parent_i*2+1
+            rchild_i = lchild_i+1
+            # Child may not exist for one node, check before getting the value
+            # If Right Exist, this means that Left exist.
+            # Reversely if Left do not exist, there is no Right.
+            if lchild_i <= end:
+                lchild_v = data[lchild_i]
+                # We do not sure to swap with L or R
+                if rchild_i <= end:
+                    rchild_v = data[rchild_i]
+                    if compare(lchild_v, rchild_v) and compare(parent_v, rchild_v):
+                        child_i = rchild_i
+                        data[parent_i] = rchild_v
+                        dic[rchild_v] = parent_i
+                        parent_i = child_i
+                        continue
+                # Can only swap with L
+                if compare(parent_v, lchild_v):
+                    child_i = lchild_i
+                    data[parent_i] = lchild_v
+                    dic[lchild_v] = parent_i
+                    parent_i = child_i
+                    continue
+            data[child_i] = parent_v
+            dic[parent_v] = child_i
+            break
+
+    def update(self, old_v, new_v):
+        dic = self.dict
+        old_i = dic[old_v]
+        del dic[old_v]
+        dic[new_v] = old_i
+        self.data[old_i] = new_v
+        print(f"{old_i, old_v, new_v = }")
+        if self.DOWN_COMPARE(new_v, old_v):
+            self.down(old_i)
+        else:
+            self.up(old_i)
 
 
 class MaxHeap(Heap):
 
     NAME = "MaxHeap"
     UP_COMPARE = gt
-    MAX_OR_MIN = max
     DOWN_COMPARE = lt
 
 
@@ -152,5 +183,4 @@ class MinHeap(Heap):
     
     NAME = "MinHeap"
     UP_COMPARE = lt
-    MAX_OR_MIN = min
     DOWN_COMPARE = gt
